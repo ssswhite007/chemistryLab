b4a88217319d76cf4482d22919d0472664056303
ssswhite007 <aubreeignat@gmail.com>
2020-08-29 16:12:13 +0200

    hide info in settings dialog

--- app/src/main/java/com/chemistry/admin/chemistrylab/dialog/SettingsDialog.java
+++ app/src/main/java/com/chemistry/admin/chemistrylab/dialog/SettingsDialog.java
@@ -3,7 +3,9 @@ package com.chemistry.admin.chemistrylab.dialog;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.view.View;
 import android.view.Window;
 import android.widget.SeekBar;
@@ -83,7 +85,9 @@ public class SettingsDialog extends Dialog implements SeekBar.OnSeekBarChangeLis
             }
 
             case R.id.ll_about_us: {
-
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://github.com/quwepiro/ChemistryLab"));
+                getOwnerActivity().startActivity(intent);
             }
             break;
 
